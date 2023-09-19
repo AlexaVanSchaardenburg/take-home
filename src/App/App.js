07a089373091bf29b228884a1b5c12027c3a8215
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-19 15:15:47 -0600

    feat: Set up componenet architecture

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import '../App/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>App</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card/Card.js
@@ -0,0 +1,11 @@
+import '../Card/Card.css';
+
+function Card() {
+  return (
+    <div className="Card">
+        <p>Card</p>
+    </div>
+  );
+}
+
+export default Card;
