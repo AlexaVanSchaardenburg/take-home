dc72506a76b5277056b35c90ab92924317793320
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-20 14:11:50 -0600

    feat: Build Card component in JSX

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,27 @@
 import '../Card/Card.css';
 
-function Card() {
-  return (
-    <div className="Card">
-        <p>Card</p>
-    </div>
-  );
+function Card({ data }) {
+
+  const checkExisits = (data) => {
+    if(!data.author || data.title === "[Removed]"){
+      return (
+        <>
+        </>
+      )
+    } else {
+      return (
+        <article className="Card">
+          <div className="title-and-img">
+           {data.urlToImage && <img className="card-img" src={`${data.urlToImage}`} />}
+           <h2 classname="card-title">{data.title}</h2>
+          </div>
+          <p className="card-date">{data.publishedAt}</p>
+          <p className="card-description">{data.description}</p>
+        </article>
+      )
+      }
+  }
+  return checkExisits(data);
 }
 
 export default Card;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,6 +7,17 @@ function Home({ data }) {
   return (
     <div className="Home">
       <Header />
+      <select name="categories" className="drop-down" id="categories-dropdown">
+        <option value="business">Business</option>
+        <option value="business">Business</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="general">General</option>
+        <option value="health">Health</option>
+        <option value="science">Science</option>
+        <option value="sports">Sports</option>
+        <option value="technology">Technology</option>
+      </select>
+      <h1>Today's Headlines</h1>
       {data.map(article => <Card data={article} />)}
     </div>
   );
