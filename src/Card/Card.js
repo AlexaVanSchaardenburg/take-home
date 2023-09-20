25053a9166418faccd76938a05e04b6311bebc8a
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-20 16:04:40 -0600

    feat: Basic styling for home view

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -13,9 +13,9 @@ function Card({ data }) {
         <article className="Card">
           <div className="title-and-img">
            {data.urlToImage && <img className="card-img" src={`${data.urlToImage}`} />}
-           <h2 classname="card-title">{data.title}</h2>
+           <p className="card-date">{data.publishedAt}</p>
+           <h2 className="card-title">{data.title}</h2>
           </div>
-          <p className="card-date">{data.publishedAt}</p>
           <p className="card-description">{data.description}</p>
         </article>
       )
